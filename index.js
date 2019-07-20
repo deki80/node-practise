9fa3d7137aadc40499a63066d09958854ec5186d
Dejan <dejan.tomic@yahoo.com>
2019-07-20 21:21:43 +0200

    parse the request url

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // dependencies
 const http = require('http')
+const url = require('url')
 
 // create a server and response to request
 const server = http.createServer((req, res) => {
+    // parse the url
+    const parsedUrl = url.parse(req.url, true)
+    const path = parsedUrl.pathname
+    
+    const trimPath = path.replace(/^\/+|\/+$/g, '')
+
+    // send the response
     res.end('Server response...')
+
+    console.log(`Requested path: ${trimPath}`)
 })
 
 //listen on port 3000
